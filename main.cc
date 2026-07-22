a55ca2a03a79f48c4f254716d1bb1b80d2d72e51
RajuuTedd <siddharthbabel082@gmail.com>
2026-07-22 20:56:10 +0530

    added the blue sky gradient

--- main.cc
+++ main.cc
@@ -1,26 +1,57 @@
 #include <iostream>
+#include "ray.h"
+#include "color.h"
+#include "vec3.h"
+
 using namespace std;
+
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
 
 int main(){
     // image 
-    int image_width =256;
-    int image_height =256;
+    
+    auto aspect_ratio = 16.0/9.0;
+    int image_width =400;
+    
+    //calculate image height for it to be at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+    
+    //camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0,0,0);
+
+    //calculate the vectors  across horizontal and down the vertical 
+    auto veiwport_u = vec3(viewport_width, 0,0);
+    auto veiwport_v = vec3( 0 ,-viewport_height ,0);
+
+    //calc the horizontal and vertical from pixel to pixel 
+    auto pixel_delta_u = veiwport_u / image_width;
+    auto pixel_delta_v = veiwport_v / image_height;
+
+    //calculate the location of the upper left pixel
+    auto veiwport_upper_left = camera_center - vec3(0,0,focal_length) - veiwport_u/2 - veiwport_v/2;
+    auto pixel00_loc = veiwport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     //render
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j = 0 ; j < image_height; j++){
+        clog<<"\rScanlines remaining: " << (image_height -j) << ' ' <<flush;
         for (int  i = 0; i < image_width; i++)
         {
-            auto r = double(i)/ (image_width-1);
-            auto g = double(j)/ (image_height-1);
-            auto b = 0.0;
-            
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            cout<< ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
         }   
-    }   
+    }
+    clog<<"\rDone.                   \n";
 }
